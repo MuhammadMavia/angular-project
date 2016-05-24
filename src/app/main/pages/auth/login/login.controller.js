25a01b928c8b8ec098741f73a216e391a7295d0e
Muhammad Mavia <Muhammadmavia172@gmail.com>
2016-05-24 15:37:01 +0500

    login complete

--- src/app/main/pages/auth/login/login.controller.js
+++ src/app/main/pages/auth/login/login.controller.js
@@ -9,15 +9,23 @@
   function LoginController($http, $location) {
     var vm = this;
     vm.doLogin = function () {
-      $location.path('/');
-      console.log(vm.user);
-      $http.post('https://www.afterbanks.com/apiapp/login/', vm.user).then(
-        function (success) {
-          console.log(success);
-        },
-        function (error) {
-          console.log(error);
-        })
+      var userData = {
+        'username': vm.user.username,
+        'password': vm.user.password
+      };
+      $.ajax({
+          type: 'POST',
+          url: 'https://www.afterbanks.com/apiapp/login/',
+          data: userData
+        }
+      ).then(
+        function (response) {
+          if (response.indexOf('"loginResult": 1,') != -1) {
+            location.assign('https://midomain.com/main/');
+          }
+          console.log(response);
+        });
     }
   }
-})();
+})
+();
